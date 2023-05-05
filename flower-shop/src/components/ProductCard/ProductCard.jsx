85156c76aee3909827373ab794f83d9c072de34e
itselenasinh <itselenagm@gmail.com>
2023-05-06 00:30:16 +0200

    working

--- flower-shop/src/components/ProductCard/ProductCard.jsx
+++ flower-shop/src/components/ProductCard/ProductCard.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { ShoppingCartContext } from "../../Context/CartContext";
+import "./ProductCard.css";
 
 import {
   Box,
@@ -10,22 +11,14 @@ import {
   IconButton,
   Typography,
 } from "@mui/material";
-import { Add, AspectRatio } from "@mui/icons-material";
+import { Add } from "@mui/icons-material";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded";
 
-// const ExpandMore = styled((props) => {
-//   const { expand, ...other } = props;
-//   return <IconButton {...other} />;
-// })(({ expand }) => ({
-//   transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-//   marginLeft: "auto",
-// }));
 
 function ProductCard({ productName, price, description, picture, stock }) {
   const navigate = useNavigate();
   const [cart, setCart] = useContext(ShoppingCartContext);
-
 
   function addToCart() {
     setCart((currProducts) => {
@@ -91,121 +84,159 @@ function ProductCard({ productName, price, description, picture, stock }) {
       <Card
         variant="outlined"
         sx={{
-          width: "360px",
-          height: "360px",
-          p: "20px",
+          width: "654px",
+          height: "420px",
+          borderRadius: "17px",
           display: "flex",
-          justifyContent: "center",
+          justifyContent: "flex-start",
           alignItems: "center",
           flexDirection: "row",
-          flexWrap: "wrap",
-          resize: "horizontal",
           overflow: "hidden",
           gap: "clamp(0px, (100% - 360px + 32px) * 999, 16px)",
           transition: "transform 0.3s, border 0.3s",
           "&:hover": {
             transform: "translateY(-2px)",
           },
-          "& > *": { minWidth: "clamp(0px, (360px - 100%) * 999,100%)" },
         }}
       >
         <CardMedia
           component="img"
-          height="194"
-          image={picture}
           alt=""
+          image={picture}
+          sx={{ height: "420px", width: "327px" }}
         ></CardMedia>
 
         <Box
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-            maxWidth: 200,
+            float: "left",
+            height: "420px",
+            width: "327px",
+            borderRadius: "17px",
+            backgroundColor: "#ffffff",
           }}
         >
-          <Box sx={{ display: "flex" }}>
-            <div>
-              <Typography
-                fontFamily="Montserrat"
-                level="h2"
-                sx={{
-                  fontSize: "x-large",
-                  pl: "20px",
-                  width: "380px",
-                  fontWeight: 700,
-                }}
-                mb={0.5}
-              >
-                {productName}
-              </Typography>
+          <Box sx={{ height: "300px", width: "327px" }}>
+            <Typography
+              fontFamily="Montserrat"
+              level="h1"
+              sx={{
+                fontSize: "34px",
+                pt: "52px",
+                m: "0 0 0 38px",
+                width: "380px",
+                color: "#474747",
+              }}
+            >
+              {productName}
+            </Typography>
 
-              {quantityPerProduct > 0 && (
+            <Typography
+              sx={{
+                h: "125px",
+                m: "0 0 0 38px",
+                color: "#8d8d8d",
+                lineHeight: "1.7em",
+                fontSize: "15px",
+                fontWeight: "lighter",
+                overflow: "hidden",
+              }}
+            >
+              {description}
+            </Typography>
+          </Box>
+
+          {/* {quantityPerProduct > 0 && (
                 <div className="product-quantity">{quantityPerProduct}</div>
               )}
-            </div>
-            <IconButton
-              size="sm"
-              variant="plain"
-              color="neutral"
-              sx={{ ml: "auto", alignSelf: "flex-start" }}
+            </div> */}
+          <Box
+            sx={{ h: "103px", w: "327px", mart: "17px", position: "relative" }}
+          >
+            <Typography
+              className="product-price"
+              sx={{
+                display: "inline-block",
+                position: "absolute",
+                top: "-13px",
+                h: "50px",
+                margin: "0 0 0 38px",
+                fontSize: "28px",
+                fontWeight: "lighter",
+                color: "#474747",
+              }}
             >
-              <FavoriteBorderRoundedIcon color="danger" />
-            </IconButton>
-          </Box>
-          <AspectRatio
-            variant="soft"
-            sx={{
-              pointerEvents: "none",
-            }}
-          >
-            <img alt="" src={picture} />
-          </AspectRatio>
-          <Typography>{description}</Typography>
-          <Typography
-            className="product-price"
-            sx={{ pl: "20px", fontWeight: 700 }}
-          >
-            {price + "€"}
-          </Typography>
-          <Box>
-            <div>
-              {quantityPerProduct === 0 ? (
-                <Button
-                  level="body2"
-                  className="product-add-button"
-                  onClick={() => addToCart()}
-                >
-                  <IconButton sx={{ backgroundColor: "none" }}>
+              {" "}
+              <span>{price + "€"}</span>
+            </Typography>
+
+            <Box>
+              <div>
+                {quantityPerProduct === 0 ? (
+                  <Button
+                    sx={{
+                      float: "right",
+                      display: "inline-block",
+                      height: "50px",
+                      width: "176px",
+                      margin: "0 40px 0 16px",
+                      boxSizing: "border-box",
+                      border: "transparent",
+                      borderRadius: "60px",
+                      fontSize: "14px",
+                      fontWeight: "500",
+                      textTransform: "uppercase",
+                      letterSpacing: "0.2em",
+                      color: "#ffffff",
+                      backgroundColor: "#9cebd5",
+                      cursor: "pointer",
+                      outline: "none",
+                    }}
+                    // level="body2"
+                    // className="product-add-button"
+                    // onClick={() => addToCart()}
+                  >BUY NOW
+                    {/* <IconButton sx={{ backgroundColor: "none" }}>
                     <AddShoppingCartIcon />
-                  </IconButton>
+                  </IconButton> */}
+                  </Button>
+                ) : (
+                  <Button
+                    className="product-plus-button"
+                    onClick={() => addToCart()}
+                  >
+                    <IconButton sx={{ backgroundColor: "none" }}>
+                      <Add />
+                    </IconButton>
+                  </Button>
+                )}
+                {quantityPerProduct > 0 && (
+                  <Button
+                    fontWeight="lg"
+                    level="body2"
+                    className="product-minus-button"
+                    onClick={() => removeProduct(productName)}
+                  >
+                    remove product
+                  </Button>
+                )}
+                <p>{stock}</p>
+                <Button onClick={() => viewProduct(productName)}>
+                  View product
                 </Button>
-              ) : (
-                <Button
-                  className="product-plus-button"
-                  onClick={() => addToCart()}
-                >
-                  <IconButton sx={{ backgroundColor: "none" }}>
-                    <Add />
-                  </IconButton>
-                </Button>
-              )}
-              {quantityPerProduct > 0 && (
-                <Button
-                  fontWeight="lg"
-                  level="body2"
-                  className="product-minus-button"
-                  onClick={() => removeProduct(productName)}
-                >
-                  remove product
-                </Button>
-              )}
-              <p>{stock}</p>
-              <Button onClick={() => viewProduct(productName)}>
-                View product
-              </Button>
-            </div>
+              </div>
+            </Box>
           </Box>
+
+          <IconButton
+            size="sm"
+            variant="plain"
+            color="neutral"
+            sx={{ ml: "auto", alignSelf: "flex-start" }}
+          >
+            <FavoriteBorderRoundedIcon color="danger" />
+          </IconButton>
+
+        
         </Box>
       </Card>
     </Box>
